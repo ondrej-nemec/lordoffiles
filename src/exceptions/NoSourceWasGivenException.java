4ac18db876faa2a1ed1c9e4137b7f2dbac15de46
Ondřej Němec <nikdynikde@gmail.com>
2018-07-07 22:25:38 +0200

    Refactor NoSourceWasGiven to NoSourceWasGivenException

--- src/exceptions/NoSourceWasGivenException.java
+++ src/exceptions/NoSourceWasGivenException.java
@@ -1,6 +1,6 @@
 package exceptions;
 
-public class NoSourceWasGiven extends NullPointerException{
+public class NoSourceWasGivenException extends NullPointerException{
 
 	private static final long serialVersionUID = 1L;
 
