c5d4a128d9eef23d30c99fe924572f5fb7777302
Ondřej Němec <nikdynikde@gmail.com>
2018-08-09 22:15:48 +0200

    WIP - BufferPlayer + test

--- src/multimedia/sound/BufferPlayer.java
+++ src/multimedia/sound/BufferPlayer.java
@@ -12,9 +12,17 @@ public class BufferPlayer extends SoundPlayerApi<SourceDataLine>{
 
 	class Play extends Thread {
 		
+		public Play() {
+			super("Buffer player");
+		}
+		
 		@Override
 		public void run() {
 			super.run();
+			play();
+		}
+		
+		private void play(){
 			int bufferSize = 128000;
 			byte[] data = new byte[bufferSize];
 			int nBytesRead = 0;
@@ -22,25 +30,29 @@ public class BufferPlayer extends SoundPlayerApi<SourceDataLine>{
 			while(nBytesRead != -1 ){
 				if(!isPaused) {
 					try {
-						while(toSkip > 0){
-							toSkip = toSkip - stream.skip(toSkip);
+						long skipped = -1;
+						while(toSkip > 0 && skipped != 0){
+							synchronize();
+							skipped = stream.skip(toSkip);
+							toSkip = toSkip - skipped;
 						}
 						nBytesRead = stream.read(data, 0, data.length);
 					} catch (IOException e) {e.printStackTrace();}
 					
 					if(nBytesRead >= 0){
 						/*int nBytesWritten =*/ line.write(data, 0, nBytesRead);
+					}else if(actualLoop < loopCount && actualLoop >= 0){
+						actualLoop++;
+						try {
+							stream.reset();
+							nBytesRead = 0;
+						} catch (IOException e) {e.printStackTrace();}
 					}
 				}
 			}
-			if(actualLoop < loopCount && actualLoop >= 0){
-				try {
-					stream.reset();
-					run();
-				} catch (IOException e) {e.printStackTrace();}
-				actualLoop++;
-			}
+			stopAfterEnd();
 		}
+		
 	}
 	
 	private int actualLoop = 0;
@@ -61,7 +73,7 @@ public class BufferPlayer extends SoundPlayerApi<SourceDataLine>{
 	protected Class<SourceDataLine> clazz() {
 		return SourceDataLine.class;
 	}
-
+	
 	@Override
 	public void play() throws LineUnavailableException, IOException {
 		throwIfNoResource();
@@ -90,18 +102,25 @@ public class BufferPlayer extends SoundPlayerApi<SourceDataLine>{
 	@Override
 	public void stop() {
 		throwIfNoResource();
-		run.interrupt();
-		run = null;
+		if(run != null){
+			run.interrupt();
+			run = null;
+		}
 		line.stop();
 		line.close();
 		isPaused = false;
+	}
+	
+	private void stopAfterEnd(){
+		stop();
 	}
 
 	@Override
 	public void foward(long msInterval) {
 		throwIfNoResource();
-		if(msInterval >= 0)
-			setPosition(getPosition() + msInterval);		
+		if(msInterval >= 0){
+			setPosition(getPosition() + msInterval);
+		}
 	}
 	
 	@Override
@@ -124,30 +143,42 @@ public class BufferPlayer extends SoundPlayerApi<SourceDataLine>{
 
 	@Override
 	public long getDuration() {
+		throwIfNoResource();
+		long pos = line.getLongFramePosition();
 		// length of stream in sample frames * actual position in micro-seconds / actual position in sample frames
-		return (long)stream.getFrameLength() * line.getMicrosecondPosition()/(long)line.getLongFramePosition();
+		return (long)stream.getFrameLength() * line.getMicrosecondPosition()/(pos==0?1:pos);
 	}
 
 	@Override
 	public void setPosition(long microSeconds) {
+		throwIfNoResource();
+		System.err.println(microSeconds + "  " + getPosition());
+		
 		if(microSeconds > getPosition()){
-			//TODO sometimes skip, sometime not
+			long pos = line.getMicrosecondPosition();
 			toSkip = microSeconds  //micro-seconds to skip
-					* line.getLongFramePosition() / (long)line.getMicrosecondPosition() //how many sample frame has one micro-second
+					* line.getLongFramePosition() / (pos==0?1:pos) //how many sample frame has one micro-second
 					* format.getSampleSizeInBits() / (long) 8; // size of sample frame in bytes
+
 		}else if(microSeconds < getPosition()){
 			//TODO
-			/*
+			//*
 			long msInterval = getPosition() - microSeconds;
 			loopCount = 1;
 			foward(getDuration() + msInterval);
-			*/
+			line.start();
+			//*/
 		}
 	}
 
 	@Override
 	public void setLoop(int count) {
+		throwIfNoResource();
 		this.loopCount = count;
+	}
+	
+	private void synchronize(){
+		
 	}
 
 }
