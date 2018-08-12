24b8d73fffbc624b6be04b64df577fc9776fe329
Ondřej Němec <nikdynikde@gmail.com>
2018-08-12 17:31:03 +0200

    PlainTextLoaderTest is mocked

--- tests/text/plaintext/PlainTextLoaderTest.java
+++ tests/text/plaintext/PlainTextLoaderTest.java
@@ -1,9 +1,11 @@
 package text.plaintext;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.Arrays;
 
 import org.junit.Test;
@@ -13,15 +15,14 @@ public class PlainTextLoaderTest {
 	
 	@Test
 	public void testReadWithConsumerWorks() {
-		try(BufferedReader br = new BufferedReader(
-				new InputStreamReader(
-						getClass().getResourceAsStream("/text/plaintext/read-consumer.txt")))){
+		try(BufferedReader br = mockBuilder("res/text/plaintext/read-consumer.txt")){
 			PlainTextLoader loader = new PlainTextLoader();
 			loader.read(br, (a)->{
 				if(a.length() == 4)
 					setTestString(a);
 			});
 			assertEquals("This is Java", stringFromConsumer);
+			verify(br, times(7)).readLine();
 		} catch (Exception e){
 			e.printStackTrace();
 			fail("IOException");
@@ -34,15 +35,14 @@ public class PlainTextLoaderTest {
 	
 	@Test
 	public void testReadAsOneStringWorks() {
-		try(BufferedReader br = new BufferedReader(
-				new InputStreamReader(
-						getClass().getResourceAsStream("/text/plaintext/read-string.txt")))){
+		try(BufferedReader br = mockBuilder("res/text/plaintext/read-string.txt")){
 			PlainTextLoader loader = new PlainTextLoader();
 			
 			assertEquals(
 					"This is text of success reading of file.\nFile has two rows;",
 					loader.readAsOneString(br)
 				);
+			verify(br, times(3)).readLine();
 		} catch (Exception e){
 			e.printStackTrace();
 			fail("IOException");
@@ -51,15 +51,14 @@ public class PlainTextLoaderTest {
 	
 	@Test
 	public void testReadLinesWorks() {
-		try(BufferedReader br = new BufferedReader(
-				new InputStreamReader(
-						getClass().getResourceAsStream("/text/plaintext/read-lines.txt")))){
+		try(BufferedReader br = mockBuilder("res/text/plaintext/read-lines.txt")){
 			PlainTextLoader loader = new PlainTextLoader();
 			
 			assertEquals(
 					Arrays.asList("First line", "Second line", "Third line"),
 					loader.read(br)
 				);
+			verify(br, times(4)).readLine();
 		} catch (Exception e){
 			e.printStackTrace();
 			fail("IOException");
@@ -68,9 +67,7 @@ public class PlainTextLoaderTest {
 	
 	@Test
 	public void testReadGridWorks() {
-		try(BufferedReader br = new BufferedReader(
-				new InputStreamReader(
-						getClass().getResourceAsStream("/text/plaintext/read-grid.txt")))){
+		try(BufferedReader br = mockBuilder("res/text/plaintext/read-grid.txt")){
 			PlainTextLoader loader = new PlainTextLoader();
 			
 			assertEquals(
@@ -81,9 +78,19 @@ public class PlainTextLoaderTest {
 							),
 					loader.read(br, "-")
 				);
+			verify(br, times(4)).readLine();
 		} catch (Exception e){
 			e.printStackTrace();
 			fail("IOException");
 		}
+	}
+	
+	private BufferedReader mockBuilder(String file) throws FileNotFoundException{
+		return mock(
+				BufferedReader.class,
+				withSettings()
+					.useConstructor(new FileReader(file))
+					.defaultAnswer(CALLS_REAL_METHODS)
+			);
 	}
 }
