fc6028fc950586dda6f6ef2383626bc5377942b2
Ondřej Němec <nikdynikde@gmail.com>
2018-12-28 00:58:12 +0100

    added source data line factory method

--- src/main/java/multimedia/sound/DataLineFactory.java
+++ src/main/java/multimedia/sound/DataLineFactory.java
@@ -5,13 +5,23 @@ import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.DataLine;
+import javax.sound.sampled.Line;
 import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.SourceDataLine;
 
 public class DataLineFactory {
 
 	public static Clip getClip(AudioInputStream stream) throws LineUnavailableException {
- 		AudioFormat format = stream.getFormat();		
- 		DataLine.Info info = new DataLine.Info(Clip.class, format);
- 		return (Clip) AudioSystem.getLine(info);
+ 		return (Clip) getLine(stream, Clip.class);
  	}
+	
+	public static SourceDataLine getSourceLine(AudioInputStream stream) throws LineUnavailableException {
+		return (SourceDataLine) getLine(stream, SourceDataLine.class);
+	}
+	
+	private static Line getLine(AudioInputStream stream, Class<?> clazz) throws LineUnavailableException {
+		AudioFormat format = stream.getFormat();
+		DataLine.Info info = new DataLine.Info(clazz, format);
+		return AudioSystem.getLine(info);
+	}
 }
