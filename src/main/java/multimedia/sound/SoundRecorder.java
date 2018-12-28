2dbf2c4eb9c2d3678f5466ae69f60a2e2b731955
Ondřej Němec <nikdynikde@gmail.com>
2018-12-28 01:23:26 +0100

    recorder api

--- src/main/java/multimedia/sound/SoundRecorder.java
+++ src/main/java/multimedia/sound/SoundRecorder.java
@@ -1,5 +1,29 @@
 package multimedia.sound;
 
+import java.util.function.Consumer;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.TargetDataLine;
+
 public class SoundRecorder {
 
+	public PlayingAction action = PlayingAction.PLAY; // TODO refactor or new enum
+	
+	private int bufferSize;
+	
+	public void capture(AudioFormat format, TargetDataLine line, Consumer<byte[]> consumer) {
+		
+	}
+	
+	public void saveSound() {
+		
+	}
+	
+	public void capcureAndSaveSound(AudioFormat format, TargetDataLine line) {
+		
+	}
+	
+	public int getBufferSize() {
+		return bufferSize;
+	}
 }
