0f32be8a6980e655973049dbbcfd46c998f6fac1
Ondřej Němec <nikdynikde@gmail.com>
2018-12-29 22:07:56 +0100

    added audio loader

--- src/main/java/multimedia/audio/AudioLoader.java
+++ src/main/java/multimedia/audio/AudioLoader.java
@@ -1,45 +1,22 @@
-package multimedia.sound;
+package multimedia.audio;
 
 import java.io.IOException;
+import java.util.function.BiConsumer;
 
 import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.SourceDataLine;
 
-public class SoundPlayer {
+public class AudioLoader {
 
-	public PlayingAction ACTION = PlayingAction.PLAY;
 	protected final int BUFFER_SIZE = 128000;
-	
-	private final AudioInputStream stream;
-	private final SourceDataLine line;
-	
-	public SoundPlayer(AudioInputStream stream, SourceDataLine line) {
-		this.stream = stream;
-		this.line = line;
-	}
-	
-	public void play() throws IOException, LineUnavailableException {
+
+	public void load(AudioInputStream stream, BiConsumer<byte[], Integer> consumer) throws IOException {
 		byte[] data = new byte[BUFFER_SIZE];
 		int nBytesRead = 0;
 		
-		line.open(stream.getFormat());
-		line.start();
-		while(nBytesRead != -1 && ACTION != PlayingAction.STOP){
-			if (ACTION == PlayingAction.PLAY) {
-				nBytesRead = stream.read(data, 0, data.length);
-				
-				if(nBytesRead >= 0){
-					/*int nBytesWritten =*/ line.write(data, 0, nBytesRead);
-				}
-			}
+		while(nBytesRead != -1){
+			nBytesRead = stream.read(data, 0, data.length);
+			consumer.accept(data, nBytesRead);
 		}
-		line.stop();
-		line.close();
-	}
-	
-	public long getMicroSecondPosition() {
-		return 0;
 	}
 	
 }
--- src/tests/java/multimedia/audio/AudioLoaderTest.java
+++ src/tests/java/multimedia/audio/AudioLoaderTest.java
@@ -1,68 +1,39 @@
-package multimedia.sound;
+package multimedia.audio;
 
+import static org.junit.Assert.fail;
 import static org.mockito.Mockito.*;
 
 import java.io.IOException;
+import java.util.function.BiConsumer;
 
 import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.SourceDataLine;
 
 import org.junit.Test;
 
-public class SoundPlayerTest {
+import multimedia.audio.AudioLoader;
+
+public class AudioLoaderTest {
 	
 	private final AudioInputStream stream;
-	private final SourceDataLine line;
-	private final SoundPlayer player;
+	private final AudioLoader loader;
 	
-	public SoundPlayerTest() {
+	public AudioLoaderTest() {
 		this.stream = mock(AudioInputStream.class);
-		this.line = mock(SourceDataLine.class);
-		this.player = new SoundPlayer(stream, line);
+		this.loader = new AudioLoader();
 	}
-
+	
 	@Test
-	public void testPlayWorksWithPlayAction() throws IOException, LineUnavailableException {
-		when(stream.read(any(), eq(0), eq(player.BUFFER_SIZE))).thenReturn(0).thenReturn(-1);
-		
-		player.play();
-		
-		verify(stream, times(2)).read(any(), eq(0), eq(player.BUFFER_SIZE));
-		verify(line, times(1)).write(any(), eq(0), anyInt());
-
-		verify(line, times(1)).open(any());
-		verify(line, times(1)).start();
-		verify(line, times(1)).close();
-		verify(line, times(1)).stop();
-		verify(stream, times(1)).getFormat();
-		
-		verifyNoMoreInteractions(stream);
-		verifyNoMoreInteractions(line);
+	public void testLoadWithConsumerWorks() {
+		try {
+			when(stream.read(any(), eq(0), eq(loader.BUFFER_SIZE))).thenReturn(0).thenReturn(-1);
+			@SuppressWarnings("unchecked")
+			BiConsumer<byte[], Integer> biConsumer = mock(BiConsumer.class);
+			
+			loader.load(stream, biConsumer);
+			verify(biConsumer, times(1)).accept(any(), eq(0));
+		} catch (IOException e) {
+			fail("IOException " + e.getMessage());
+		}
 	}
 
-	//TODO how to test this infinity cyklus
-	//@Test
-	public void testPlayWorksWithPauseAction() throws IOException, LineUnavailableException {
-		player.ACTION = PlayingAction.PAUSE;
-		player.play();
-		verifyZeroInteractions(stream);
-		verifyZeroInteractions(line);
-	}
-
-	@Test
-	public void testPlayWorksWithStopAction() throws IOException, LineUnavailableException {
-		player.ACTION = PlayingAction.STOP;
-		
-		player.play();
-		
-		verify(line, times(1)).open(any());
-		verify(line, times(1)).start();
-		verify(line, times(1)).close();
-		verify(line, times(1)).stop();
-		verify(stream, times(1)).getFormat();
-		
-		verifyNoMoreInteractions(stream);
-		verifyNoMoreInteractions(line);
-	}
 }
