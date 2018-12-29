02d25bd20080322c7778c991b13202cd7e4eed84
Ondřej Němec <nikdynikde@gmail.com>
2018-12-29 22:05:49 +0100

    Microphone implemented

--- src/main/java/multimedia/sound/Microphone.java
+++ src/main/java/multimedia/sound/Microphone.java
@@ -1,26 +1,49 @@
 package multimedia.sound;
 
+import java.io.ByteArrayOutputStream;
 import java.util.function.Consumer;
 
 import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.TargetDataLine;
 
-public class SoundRecorder {
+public class Microphone {
 
-	public PlayingAction action = PlayingAction.PLAY; // TODO refactor or new enum
+	public boolean capture = true;
 	
 	private int bufferSize;
 	
-	public void capture(AudioFormat format, TargetDataLine line, Consumer<byte[]> consumer) {
-		
+	public void capture(TargetDataLine line, AudioFormat format, Consumer<byte[]> consumer) throws LineUnavailableException {
+		line.open(format);
+	    
+	    int numBytesRead = 0;
+	    byte[] data = new byte[line.getBufferSize() / 5];
+	    
+	    line.start();
+	    while (numBytesRead != -1 && capture) {
+		   numBytesRead =  line.read(data, 0, data.length);
+		   consumer.accept(data);
+	    }
+	    line.stop();
+	    line.drain();
+	    line.close();
 	}
-	
-	public void saveSound() {
 		
-	}
-	
-	public void capcureAndSaveSound(AudioFormat format, TargetDataLine line) {
-		
+	public void capture(TargetDataLine line, AudioFormat format, ByteArrayOutputStream out) throws LineUnavailableException {
+		line.open(format);
+	    
+	    int numBytesRead = 0;
+	    byte[] data = new byte[line.getBufferSize() / 5];
+	    
+	    line.start();
+	    while (numBytesRead != -1 && capture) {
+		   numBytesRead =  line.read(data, 0, data.length);
+		   if (numBytesRead >= 0)
+			   out.write(data, 0, bufferSize);
+	    }
+	    line.stop();
+	    line.drain();
+	    line.close();
 	}
 	
 	public int getBufferSize() {
--- src/tests/java/multimedia/sound/MicrophoneTest.java
+++ src/tests/java/multimedia/sound/MicrophoneTest.java
@@ -1,5 +1,72 @@
 package multimedia.sound;
 
-public class SoundRecorderTest {
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.*;
 
+import java.io.ByteArrayOutputStream;
+import java.util.function.Consumer;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.TargetDataLine;
+
+import org.junit.Test;
+
+public class MicrophoneTest {
+	
+	private final TargetDataLine line;
+	private final AudioFormat format;
+	private final Microphone mic;
+	
+	public MicrophoneTest() {
+		this.line = mock(TargetDataLine.class);
+		this.format = mock(AudioFormat.class);
+		this.mic = new Microphone();
+	}
+
+	@Test
+	public void testCaptureWithConsumerWorks() {
+		try {
+			when(line.read(any(), eq(0), eq(mic.getBufferSize()))).thenReturn(1).thenReturn(-1);
+			
+			@SuppressWarnings("unchecked")
+			Consumer<byte[]> consumer = mock(Consumer.class);
+			
+			mic.capture(line, format, consumer);
+			
+			verify(consumer, times(2)).accept(any());
+			verify(line, times(1)).open(format);
+			verify(line, times(1)).start();
+			verify(line, times(1)).stop();
+			verify(line, times(1)).drain();
+			verify(line, times(1)).close();
+			
+			
+		} catch (LineUnavailableException e) {
+			fail("LineUnavailableException " + e.getMessage());
+		}
+	}
+	
+	@Test
+	public void testCaptureWithArrayWorks() {
+		try {
+			when(line.read(any(), eq(0), eq(mic.getBufferSize()))).thenReturn(1).thenReturn(-1);
+			
+			ByteArrayOutputStream output = mock(ByteArrayOutputStream.class);
+			
+			mic.capture(line, format, output);
+			
+			verify(output, times(1)).write(any(), eq(0), eq(mic.getBufferSize()));
+			verify(line, times(1)).open(format);
+			verify(line, times(1)).start();
+			verify(line, times(1)).stop();
+			verify(line, times(1)).drain();
+			verify(line, times(1)).close();
+			
+			
+		} catch (LineUnavailableException e) {
+			fail("LineUnavailableException " + e.getMessage());
+		}
+	}
+	
 }
